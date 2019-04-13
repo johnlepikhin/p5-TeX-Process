0cd7b00dbd1971c5196c62e73176c5a624da6b16
Evgenii Lepikhin <johnlepikhin@gmail.com>
2019-04-13 21:17:15 +0300

    Init 'flatten' (plaintext) printer

--- lib/TeX/Processor/Printer.pm
+++ lib/TeX/Processor/Printer.pm
@@ -93,4 +93,89 @@ sub latex {
     return $r;
 }
 
+sub flatten {
+    my $document = get_arg document => @_;
+
+    my $printer;
+    $printer = sub {
+        my $lexema = shift;
+
+        if ( $lexema->{type} eq 'TEXT' ) {
+            if ( $lexema->{content} eq '&' ) {
+                return "&";
+            }
+            if ( $lexema->{content} eq '$' ) {
+                return "\$";
+            }
+            if ( $lexema->{content} eq '{' ) {
+                return "{";
+            }
+            if ( $lexema->{content} eq '}' ) {
+                return "}";
+            }
+            my $r = $lexema->{content};
+            $r =~ s{~}{ }g;
+            return $r;
+        }
+
+        if ( $lexema->{type} eq 'TILDA' ) {
+            return q{ };
+        }
+        if ( $lexema->{type} eq 'HASH' ) {
+            return q{#};
+        }
+        if ( $lexema->{type} eq 'CARET' ) {
+            return q{^};
+        }
+        if ( $lexema->{type} eq 'AMPERSAND' ) {
+            return q{&};
+        }
+        if ( $lexema->{type} eq 'UNDERSCORE' ) {
+            return q{_};
+        }
+        if ( $lexema->{type} eq 'L_SQ_BRACKET' ) {
+            return q{[};
+        }
+        if ( $lexema->{type} eq 'R_SQ_BRACKET' ) {
+            return q{]};
+        }
+
+        if ( $lexema->{type} eq 'COMMENT' ) {
+            return q{};
+        }
+
+        if ( $lexema->{type} eq 'GROUP' ) {
+            return join q{}, map { $printer->($_) } @{ $lexema->{children} };
+        }
+        if ( $lexema->{type} eq 'MATH' ) {
+            return '$some formula here$';
+        }
+
+        if ( $lexema->{type} eq 'COMMAND' ) {
+            my $r = q{};
+            foreach my $arg ( @{ $lexema->{args} } ) {
+                my $content = join q{}, map { $printer->($_) } @{ $arg->{content} };
+                if ( $arg->{type} eq 'brace' ) {
+                    $r .= " $content";
+                    next;
+                }
+                if ( $arg->{type} eq 'bracket' ) {
+                    $r .= " $content";
+                    next;
+                }
+            }
+            return $r;
+        }
+
+        confess "Unknown lexema type $lexema->{type}";
+    };
+
+    my $r = q{};
+    foreach ( @{$document} ) {
+        $r .= $printer->($_);
+    }
+
+    return $r;
+}
+
 1;
